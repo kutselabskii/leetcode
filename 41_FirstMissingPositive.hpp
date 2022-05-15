3646200ac88c944b4c83af2ba20e9514b27e52f8
Egor Kutselabskii <osutanfirst@gmail.com>
2022-05-15 23:33:53 +0300

    Add 41 time-worsened, memory-optimized solution

--- 41_FirstMissingPositive.hpp
+++ 41_FirstMissingPositive.hpp
@@ -4,27 +4,28 @@
 #include <unordered_set>
 
 // Time: O(n)
-// Memory: O(1), but we need 500k elements bool array
+// Memory: O(1), not using additional arrays
 
 int firstMissingPositive(std::vector<int>& nums)
 {
-	const int maxAmount = 500000 + 1;
-
-	bool metValues[maxAmount] = {false};
-
-	for (int& num : nums) {
-		if (num < 0 || num >= maxAmount) {
-			continue;
-		}
-
-		metValues[num] = true;
-	}
-
-	for (int i = 1; i < maxAmount; ++i) {
-		if (metValues[i] == false) {
-			return i;
+	int n = nums.size();
+	for (int i = 0; i < n; ++i) {
+		if (nums[i] <= 0) {
+			nums[i] = n + 1;
 		}
 	}
 
-	return maxAmount;
+	for (int i = 0; i < n; ++i) {
+		if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) {
+			nums[abs(nums[i]) - 1] *= -1;
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		if (nums[i] > 0) {
+			return i + 1;
+		}
+	}
+
+	return n + 1;
 }
